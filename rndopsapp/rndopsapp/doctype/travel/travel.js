513b58c45d4d9bb17e1816121cc6e197d72ed8c4
rndops <priyam96>
2026-04-01 22:14:42 +0530

    Update of travel

--- rndopsapp/rndopsapp/doctype/travel/travel.js
+++ rndopsapp/rndopsapp/doctype/travel/travel.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2025, rndops and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Travel", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Travel", {
+	travel_project_title(frm) {
+		if (frm.doc.travel_project_title) {
+			frappe.db.get_value(
+				"Project Registration",
+				frm.doc.travel_project_title,
+				"project_no",
+				(r) => {
+					frm.set_value("travel_project_number", r && r.project_no ? r.project_no : "");
+				}
+			);
+		} else {
+			frm.set_value("travel_project_number", "");
+		}
+	},
+});
